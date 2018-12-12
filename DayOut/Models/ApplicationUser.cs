892c380332fda63891dc1a9be9679e788ac3bd9b
Lagrango <davidnlagrange@hotmail.com>
2018-12-12 09:38:13 -0600

    Added Roles Functionality

---
+++ DayOut/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DayOut.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [NotMapped]
+        public bool isSuperAdmin { get; set; }
+    }
+}
---
+++ DayOut/StaticDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DayOut
+{
+    public class StaticDetails
+    {
+        public const string Customer = "Customer";
+        public const string AdminEndUser = "Admin";
+        public const string SuperAdminEndUser = "Super Admin";
+    }
+}
