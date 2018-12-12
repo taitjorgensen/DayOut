5d16b309b6b8d39cce1a19c82888f8fcebb04300
Lagrango <davidnlagrange@hotmail.com>
2018-12-12 12:25:58 -0600

    Added Customer Controller and Scaffolding

--- DayOut/Data/ApplicationDbContext.cs
+++ DayOut/Data/ApplicationDbContext.cs
@@ -9,9 +9,9 @@ namespace DayOut.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        DbSet<ApplicationUser> ApplicationUsers { get; set; }
-        DbSet<Customer> Customers { get; set; }
-        DbSet<State> States { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<State> States { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
