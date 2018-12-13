ce9b57cedaa351508b2047551b6351bc2fca8486
Lagrango <davidnlagrange@hotmail.com>
2018-12-13 07:35:18 -0600

    Added Category and Choice Models

---
+++ DayOut/Models/Choice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DayOut.Models
+{
+    public class Choice
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public DateTime CurrentDay { get; set; }
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
